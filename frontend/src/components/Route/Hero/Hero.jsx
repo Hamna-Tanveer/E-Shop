715bf2d799a36624b6cfa139829502435106e95a
Hamna-Tanveer <hamnatanveer345@gmail.com>
2025-10-28 03:09:55 +0500

    complete categories section of Home page

--- frontend/src/components/Route/Hero/Hero.jsx
+++ frontend/src/components/Route/Hero/Hero.jsx
@@ -1,4 +1,3 @@
-import React from "react";
 import styles from "../../../styles/styles";
 import { Link } from "react-router-dom";
 export const Hero = () => {
--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,10 +1,13 @@
 import Header from "../components/Layout/Header";
 import { Hero } from "../components/Route/Hero/Hero.jsx";
+import Categories from "../components/Route/Categories/Categories.jsx";
+
 function HomePage() {
   return (
     <div>
       <Header activeHeading={1} />
       <Hero />
+      <Categories />
     </div>
   );
 }
